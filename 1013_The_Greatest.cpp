5ebcdf41ff93d971af7ccdccd1fb9ed3e0fe9b6d
ummay-salma10 <tajinsultana698@gmail.com>
2026-06-03 15:53:33 +0600

    Create 1013_The_Greatest.cpp

---
+++ 1013_The_Greatest.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int a,b,s;
+ cin>>a>>b>>s;
+ int MaiorAB=(a+b+abs(a-b))/2;
+ int m=(MaiorAB+s+abs(MaiorAB-s))/2;
+ cout<<m<<" "<<"eh o maior"<<endl;
+ return 0;
+}
