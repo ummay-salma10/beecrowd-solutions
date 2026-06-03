a6651801ad69c727701a9a38a0ea8f7195b238d8
ummay-salma10 <tajinsultana698@gmail.com>
2026-06-04 02:04:25 +0600

    Added beecrowd 1014

---
+++ 1014_Consumption.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int X;
+ cin>>X;
+ float Y;
+ cin>>Y;
+ double t=X/Y;
+ cout<<fixed<<setprecision(3)<<t<<" "<<"km/l"<<endl;
+ return 0;
+
+
+}
